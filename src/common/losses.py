904e9152fcbaada780622b12ef86466b2829ace0
joonleesky <joonleesky90@gmail.com>
2022-07-17 10:46:30 +0000

    temporal consistency loss

--- src/common/losses.py
+++ src/common/losses.py
@@ -62,19 +62,77 @@ class TemporalConsistencyLoss(nn.Module):
     # Temporal extension of BYOL loss
     # Identical to the BYOL loss if T=1
 
-    def __init__(self):
+    def __init__(self, time_span, num_trajectory, device):
         super().__init__()
+        self.time_span = time_span
+        self.num_trajectory = num_trajectory
+        self.device = device
 
-    def forward(self, p, z):
-        # T: trajectory_size, B: batch_size, D: representation_dim
-        # p, z: (T, B, D)
+    def forward(self, p, z, done):
+        # T: trajectory_size, N: batch_size, D: representation_dim
+        # [params] p, z: (T*N, D)
+        # [params] done: (T, N)
+        T, N = self.time_span, self.num_trajectory
 
+        # p: (T*N,D)->(T,N,D)->(N,T,D)
+        # z: (T*N,D)->(T,N,D)->(N,D,T)
+        p = F.normalize(p, dim=-1)
+        z = F.normalize(z, dim=-1)
+        p = p.view(T, N, -1).permute(1,0,2)
+        z = z.view(T, N, -1).permute(1,2,0)
+        # logits: (N, T, T)
+        logits = torch.matmul(p, z)
+
+        # done_mask: do not select the logits after done (different trajectory)
+        done = done.float().T
+        done_idx = torch.nonzero(done==1)
+        for idx in done_idx:
+            row = idx[0]
+            col = idx[1]
+            done[row, col] = 0
+            done[row, col+1:] = 1
+        done_mask = 1-done.float()
+        done_mask = torch.matmul(done_mask.unsqueeze(2), done_mask.unsqueeze(1))        
+        done_mask = 1 - done_mask
+
+        # compute mean of the similarity
+        loss = (1-done_mask) * logits
+        loss = -torch.mean(logits)
+        
         return loss
-
-    
-
 
 
 if __name__ == '__main__':
-    print('[TEST LOSS Functions]')
+    print('[TEST Loss Functions]')
+    N, T, D = 2, 3, 2
+    device = torch.device('cuda:0')
+    z1 = torch.FloatTensor([[[0.0, 1.0],
+                             [1.0, 0.0],
+                             [0.707, 0.707]],
+                            
+                            [[-1.0, 1.0],
+                             [-1.0, 0.0],
+                             [-0.707, 0.707]],]).to(device).permute(1,0,2)
 
+    z2 = torch.FloatTensor([[[1.0, 0.0],
+                             [0.707, -0.707],
+                             [0.707, 0.707]],
+                            
+                            [[-1.0, 0.0],
+                             [-0.5, 0.707],
+                             [-1.0, 0.0]],]).to(device).permute(1,0,2)
+
+    z1 = z1.reshape(T*N, D)
+    z2 = z2.reshape(T*N, D)
+    z = torch.cat([z1, z2])
+
+    print('[1. TEST Temporal Contrastive Loss]')
+    print('[1.1 TEST loss without done]')
+    loss_fn = TemporalContrastiveLoss(time_span=T, 
+                                      num_trajectory=N, 
+                                      temperature=1.0, 
+                                      device=device)
+    done = torch.zeros((3,2)).to(device)
+    loss = loss_fn(z, done)
+    import pdb
+    pdb.set_trace()
