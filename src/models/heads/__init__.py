53025746da669cff7bbb26fd6193b02ba809b85d
joonleesky <joonleesky90@gmail.com>
2022-09-01 14:52:24 +0900

    impala arch modified

--- src/models/heads/__init__.py
+++ src/models/heads/__init__.py
@@ -5,7 +5,8 @@ from .curl_head import CURLHead
 from .mlr_head import MLRHead
 from .vit_head import VITHead
 from .drloc_head import DRLocHead
+from .vqvae_head import VQVAEHead
 
 __all__ = [
-    'BaseHead', 'BYOLHead', 'SimCLRHead', 'CURLHead', 'MLRHead', 'VITHead', 'DRLocHead'
+    'BaseHead', 'BYOLHead', 'SimCLRHead', 'CURLHead', 'MLRHead', 'VITHead', 'DRLocHead', 'VQVAEHead'
 ]
