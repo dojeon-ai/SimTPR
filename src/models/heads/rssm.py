98b98241f7969d6e26ac0132fa8aa4ee99d2ad0b
koanho <le5544@korea.ac.kr>
2022-12-22 11:48:17 +0000

    add rssm head done before rssm head

--- src/models/heads/rssm.py
+++ src/models/heads/rssm.py
@@ -205,26 +205,3 @@ class ObsModel(nn.Module):
         x = x.reshape(x.shape[0], 64, 7, 7)
         x = self.layers(x)
         return x
-
-class ObsModel(nn.Module):
-    """
-    p(o_t | s_t, h_t)
-    Observation model to reconstruct image observation (3, 64, 64)
-    from state and rnn hidden state
-    """
-    def __init__(self, state_dim, rnn_hidden_dim):
-        super(ObsModel, self).__init__()
-        self.fc = nn.Linear(state_dim + rnn_hidden_dim, 1024)
-        self.dc1 = nn.ConvTranspose2d(1024, 128, kernel_size=5, stride=2)
-        self.dc2 = nn.ConvTranspose2d(128, 64, kernel_size=5, stride=2)
-        self.dc3 = nn.ConvTranspose2d(64, 32, kernel_size=6, stride=2)
-        self.dc4 = nn.ConvTranspose2d(32, 3, kernel_size=6, stride=2)
-
-    def forward(self, state, rnn_hidden):
-        hidden = self.fc(torch.cat([state, rnn_hidden], dim=1))
-        hidden = hidden.view(hidden.size(0), 1024, 1, 1)
-        hidden = F.relu(self.dc1(hidden))
-        hidden = F.relu(self.dc2(hidden))
-        hidden = F.relu(self.dc3(hidden))
-        obs = self.dc4(hidden)
-        return obs
