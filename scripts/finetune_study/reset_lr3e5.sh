82249bc6591c990841786d680baf75fd93b51ddb
joonleesky <joonleesky90@gmail.com>
2022-12-29 19:38:38 +0900

    ft scrfipt

--- scripts/finetune_study/reset_lr3e5.sh
+++ scripts/finetune_study/reset_lr3e5.sh
@@ -2,11 +2,11 @@ cd ..
 cd ..
 python run_atari_finetune.py \
     --group_name ft_study \
-    --exp_name gpt_video_reset_trainbb-eval_lr3e5 \
+    --exp_name gpt_video_reset_tbb-eval_lr3e5_reset20000 \
     --mode full \
     --config_name gpt_impala \
     --num_seeds 3 \
-    --num_devices 2 \
+    --num_devices 8 \
     --num_exp_per_device 2 \
     --overrides pretrain.artifact_name='atari_pretrain/gpt_video_cons_reg001_nproj:latest' \
     --overrides agent.finetune_type='naive' \
@@ -14,4 +14,4 @@ python run_atari_finetune.py \
     --overrides agent.train_backbone_mode='eval' \
     --overrides agent.train_target_backbone_mode='eval' \
     --overrides agent.optimizer.lr=0.00003 \
-    --overrides agent.reset_freq=10000
+    --overrides agent.reset_freq=20000
--- scripts/finetune_study/reset_lr3e5_tbb-train.sh
+++ scripts/finetune_study/reset_lr3e5_tbb-train.sh
@@ -2,7 +2,7 @@ cd ..
 cd ..
 python run_atari_finetune.py \
     --group_name ft_study \
-    --exp_name gpt_video_reset_trainbb-eval_lr1e4 \
+    --exp_name gpt_video_reset_tbb-train_lr3e5_reset20000 \
     --mode full \
     --config_name gpt_impala \
     --num_seeds 3 \
@@ -11,7 +11,7 @@ python run_atari_finetune.py \
     --overrides pretrain.artifact_name='atari_pretrain/gpt_video_cons_reg001_nproj:latest' \
     --overrides agent.finetune_type='naive' \
     --overrides agent.eval_backbone_mode='eval' \
-    --overrides agent.train_backbone_mode='eval' \
+    --overrides agent.train_backbone_mode='train' \
     --overrides agent.train_target_backbone_mode='eval' \
-    --overrides agent.optimizer.lr=0.0001 \
+    --overrides agent.optimizer.lr=0.00003 \
     --overrides agent.reset_freq=20000
